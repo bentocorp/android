a6559af7972698364fb8a5a28d5985796f662a23
Norman Paniagua <normanpaniagua@gmail.com>
2015-07-14 14:15:46 -0400

    Preview next menu is now tappable

--- app/src/main/java/com/bentonow/bentonow/NextDayMenuActivity.java
+++ app/src/main/java/com/bentonow/bentonow/NextDayMenuActivity.java
@@ -3,7 +3,6 @@ package com.bentonow.bentonow;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,9 +15,6 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.bentonow.bentonow.model.Dish;
-import com.bentonow.bentonow.model.Ioscopy;
-import com.bentonow.bentonow.model.Item;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
@@ -27,7 +23,6 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 
 public class NextDayMenuActivity extends BaseActivity {
@@ -37,6 +32,10 @@ public class NextDayMenuActivity extends BaseActivity {
     private LayoutInflater inflater;
     private String title;
     private Activity _this;
+    private String activeRow = "";
+    OIListAdapter sideAdapter;
+    Holder[] mainListItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,22 +61,34 @@ public class NextDayMenuActivity extends BaseActivity {
     private static class Holder {
         public ImageView img;
         public TextView main_title;
-        public ImageView img_2;
-        public TextView main_title_2;
+        public TextView desc_title;
+        public RelativeLayout overlay_menu_detail;
+        public TextView main_menu_item_description;
+
     }
 
     private void listItems(JSONArray menuItems) {
         LinearLayout list = (LinearLayout)findViewById(R.id.tomorrow_main_dishes_container);
+        mainListItems = new Holder[menuItems.length()];
         for( int i = 0; i < menuItems.length(); i++ ){
             try {
                 JSONObject row = menuItems.getJSONObject(i);
+
                 if(row.getString("type").equals("main")){
                     View vi = inflater.inflate(R.layout.inc_menu_item_side, null);
                     Holder holder = new Holder();
                     holder.img = (ImageView) vi.findViewById(R.id.menu_item_image);
                     holder.main_title = (TextView) vi.findViewById(R.id.main_menu_item_name);
+                    holder.desc_title = (TextView) vi.findViewById(R.id.main_menu_item_name_2);
+                    holder.overlay_menu_detail = (RelativeLayout) vi.findViewById(R.id.overlay_menu_detail);
+                    holder.main_menu_item_description = (TextView)vi.findViewById(R.id.main_menu_item_description);
+                    vi.findViewById(R.id.btn_added).setVisibility(View.GONE);
+                    vi.findViewById(R.id.btn_add_to_bento).setVisibility(View.GONE);
+                    vi.findViewById(R.id.btn_add_to_bento_solded).setVisibility(View.GONE);
 
                     holder.main_title.setText(row.getString(Config.DISH.NAME).toUpperCase());
+                    holder.desc_title.setText(row.getString(Config.DISH.NAME).toUpperCase());
+                    holder.main_menu_item_description.setText(row.getString(Config.DISH.DESCRIPTION).toUpperCase());
                     if ( row.getString(Config.DISH.IMAGE1)!=null && !row.getString(Config.DISH.IMAGE1).isEmpty() ) {
                         Picasso.with(getApplicationContext())
                                 .load(row.getString(Config.DISH.IMAGE1))
@@ -86,7 +97,27 @@ public class NextDayMenuActivity extends BaseActivity {
                                 .into(holder.img);
                     }
 
+                    final int position = i;
+
+                    holder.main_title.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            notifyAllDataSetChanged("m" + position);
+                        }
+                    });
+
+                    holder.overlay_menu_detail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            notifyAllDataSetChanged("");
+                        }
+                    });
+
+                    mainListItems[i] = holder;
+
                     list.addView(vi);
+                } else {
+                    mainListItems[i] = null;
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -94,6 +125,23 @@ public class NextDayMenuActivity extends BaseActivity {
         }
 
         getSideList();
+    }
+
+    private void notifyAllDataSetChanged (String row) {
+        activeRow = row;
+
+        sideAdapter.notifyDataSetChanged();
+
+        for (int i=0; i < mainListItems.length; ++i) {
+            if (mainListItems[i] == null) continue;
+            else if (activeRow.equals("m" + i)) {
+                mainListItems[i].main_title.setVisibility(View.GONE);
+                mainListItems[i].overlay_menu_detail.setVisibility(View.VISIBLE);
+            } else {
+                mainListItems[i].main_title.setVisibility(View.VISIBLE);
+                mainListItems[i].overlay_menu_detail.setVisibility(View.GONE);
+            }
+        }
     }
 
     private void initActionbar() {
@@ -113,15 +161,15 @@ public class NextDayMenuActivity extends BaseActivity {
     }
 
     public void getSideList(){
-        ArrayList<HashMap<String, String>> sideMenuList = new ArrayList<HashMap<String, String>>();
+        ArrayList<HashMap<String, String>> sideMenuList = new ArrayList<>();
 
         for( int i = 0; i < MenuItems.length(); i++ ){
             try {
                 JSONObject row = MenuItems.getJSONObject(i);
-                View vi = inflater.inflate(R.layout.inc_menu_item_side, null);
                 if(row.getString("type").equals("side")){
-                    HashMap<String, String> map = new HashMap<String, String>();
+                    HashMap<String, String> map = new HashMap<>();
                     map.put(Config.DISH.NAME, row.getString(Config.DISH.NAME).toUpperCase());
+                    map.put(Config.DISH.DESCRIPTION, row.getString(Config.DISH.DESCRIPTION));
                     map.put(Config.DISH.IMAGE1, row.getString(Config.DISH.IMAGE1));
                     sideMenuList.add(map);
                 }
@@ -131,9 +179,9 @@ public class NextDayMenuActivity extends BaseActivity {
             }
         }
 
-        GridView CompanyListView = (GridView) findViewById(R.id.menu_item_side_listview);
-        OIListAdapter adapter = new OIListAdapter(_this, sideMenuList);
-        CompanyListView.setAdapter(adapter);
+        GridView SideListView = (GridView) findViewById(R.id.menu_item_side_listview);
+        sideAdapter = new OIListAdapter(_this, sideMenuList);
+        SideListView.setAdapter(sideAdapter);
     }
 
     public class OIListAdapter extends BaseAdapter {
@@ -141,17 +189,13 @@ public class NextDayMenuActivity extends BaseActivity {
         private static final String TAG = "OIListAdapter";
         private final ArrayList<HashMap<String, String>> data;
         private final Activity activity;
-        //private final AQuery listAq;
         private final LayoutInflater inflater;
-        private Holder current_holder;
-        //private HashMap<String, String> row;
 
 
         public OIListAdapter(Activity a, ArrayList<HashMap<String, String>> datos) {
             data = datos;
             activity = a;
             inflater = (LayoutInflater) a.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            //listAq = new AQuery(activity);
         }
 
         public int getCount() {
@@ -165,19 +209,19 @@ public class NextDayMenuActivity extends BaseActivity {
         public long getItemId(int i) {
             return i;
         }
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             View view = convertView;
             Holder holder = new Holder();
             if (view == null) {
                 view = inflater.inflate(R.layout.inc_menu_item_side, null);
-                holder.container = (RelativeLayout)view.findViewById(R.id.container);
                 holder.img = (ImageView) view.findViewById(R.id.menu_item_image);
                 holder.main_title = (TextView) view.findViewById(R.id.main_menu_item_name);
                 holder.desc_title = (TextView) view.findViewById(R.id.main_menu_item_name_2);
                 holder.overlay_menu_detail = (RelativeLayout) view.findViewById(R.id.overlay_menu_detail);
                 holder.main_menu_item_description = (TextView)view.findViewById(R.id.main_menu_item_description);
-                //holder.col1_solded_flag = (ImageView)view.findViewById(R.id.col1_solded_flag);
-                holder.btn_added = (LinearLayout)view.findViewById(R.id.btn_added);
+                view.findViewById(R.id.btn_added).setVisibility(View.GONE);
+                view.findViewById(R.id.btn_add_to_bento).setVisibility(View.GONE);
+                view.findViewById(R.id.btn_add_to_bento_solded).setVisibility(View.GONE);
                 view.setTag(holder);
             } else {
                 holder = (Holder) view.getTag();
@@ -186,15 +230,18 @@ public class NextDayMenuActivity extends BaseActivity {
             try {
                 holder.row = data.get(position);
                 Log.i(TAG, "row: " + holder.row.toString());
-                final Holder finalHolder = holder;
                 String title = "";
+                String description = "";
                 try {
                     title = holder.row.get(Config.DISH.NAME).toUpperCase();
+                    description = holder.row.get(Config.DISH.DESCRIPTION);
                 }catch (NullPointerException ignored){
 
                 }
 
                 holder.main_title.setText(title);
+                holder.desc_title.setText(title);
+                holder.main_menu_item_description.setText(description);
 
                 if(!holder.row.get(Config.DISH.IMAGE1).isEmpty()) {
                     Picasso.with(activity)
@@ -203,6 +250,27 @@ public class NextDayMenuActivity extends BaseActivity {
                             .error(R.drawable.tmp_trans)
                             .into(holder.img);
                 }
+
+                if (activeRow.equals("s" + position)) {
+                    holder.main_title.setVisibility(View.GONE);
+                    holder.overlay_menu_detail.setVisibility(View.VISIBLE);
+                } else {
+                    holder.main_title.setVisibility(View.VISIBLE);
+                    holder.overlay_menu_detail.setVisibility(View.GONE);
+                }
+
+                holder.main_title.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        notifyAllDataSetChanged("s" + position);
+                    }
+                });
+                holder.overlay_menu_detail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        notifyAllDataSetChanged("");
+                    }
+                });
             }catch (IndexOutOfBoundsException ignore){
                 Log.i(TAG,"Col 1 empty");
                 holder.main_title.setText("");
@@ -213,17 +281,12 @@ public class NextDayMenuActivity extends BaseActivity {
         }
 
         private class Holder {
-            public boolean selected = false;
-            public Boolean pressed = false;
             public ImageView img;
             public TextView main_title;
             public TextView desc_title;
             public RelativeLayout overlay_menu_detail;
             public TextView main_menu_item_description;
-            public ImageView col1_solded_flag;
-            public RelativeLayout container;
             public HashMap<String, String> row;
-            public LinearLayout btn_added;
         }
     }
 
