20428a1f5f440381e7f300d51ea4ebe10db6e56f
Norman Paniagua <normanpaniagua@gmail.com>
2015-07-28 17:30:15 -0400

    Added application to check if application is on foreground or background

---
+++ app/src/main/java/com/bentonow/bentonow/BentoApplication.java
@@ -0,0 +1,67 @@
+package com.bentonow.bentonow;
+
+import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
+
+import com.orm.SugarApp;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BentoApplication extends SugarApp {
+
+    private static final String TAG = "BentoApplication";
+
+    static Timer timer;
+    static TimerTask stopService;
+    static BentoApplication instance = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        instance = this;
+    }
+
+    static public void onPause () {
+        timer = new Timer();
+
+        Log.i(TAG, "onPause");
+
+        stopService = new TimerTask() {
+            @Override
+            public void run() {
+                Log.i(TAG, "stoping service");
+                try {
+                    instance.stopService(new Intent(instance, BentoService.class));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // After timer reached the service will be stop
+        timer.schedule(stopService, 10 * 1000);
+    }
+
+    static public void onResume () {
+        Log.i(TAG, "onResume");
+
+        if (timer != null) {
+            Log.i(TAG, "cancel timer");
+            timer.cancel();
+            timer = null;
+        }
+
+        if (!BentoService.isRunning() && instance != null) {
+            Log.i(TAG, "starting service");
+            try {
+                instance.startService(new Intent(instance, BentoService.class));
+                BentoService.forceCheckAll();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
