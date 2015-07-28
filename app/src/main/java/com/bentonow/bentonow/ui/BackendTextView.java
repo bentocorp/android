e00d0f06bf38ed9b963a0a05cd5d040fd6907110
Norman Paniagua <normanpaniagua@gmail.com>
2015-07-28 18:05:24 -0400

    Added backend text support to closed view

--- app/src/main/java/com/bentonow/bentonow/ui/BackendTextView.java
+++ app/src/main/java/com/bentonow/bentonow/ui/BackendTextView.java
@@ -1,7 +1,40 @@
 package com.bentonow.bentonow.ui;
 
-/**
- * Created by normanpaniagua on 7/28/15.
- */
-public class BackendTextView {
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.bentonow.bentonow.R;
+import com.bentonow.bentonow.model.Ioscopy;
+
+public class BackendTextView extends TextView {
+
+    public BackendTextView(Context context) {
+        super(context);
+    }
+
+    public BackendTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public BackendTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    void init (Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.BackendTextView,
+                0, 0);
+
+        try {
+            String key = a.getString(R.styleable.BackendTextView_key);
+            setText(Ioscopy.getKeyValue(key));
+        } finally {
+            a.recycle();
+        }
+    }
 }
